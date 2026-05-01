4384a56c7b1f2893295e92c3771503083e777bfe
Amy Jeanes <amyjeanes23@gmail.com>
2026-05-01 04:20:31 +0200

    Revert "Add temporary diagnostic probes to verify SARIF pipeline"
    This reverts commit 4d9bdde6d0db1bf80516dd58184c77dfc391540c.

--- lua/tardis/parts/events.lua
+++ lua/tardis/parts/events.lua
@@ -12,12 +12,8 @@ PART.InvisibleFade = true
 PART.FadeSpeed = 2
 PART.AllowThroughPortals = true
 
----@type number
-local lspWarningProbe = "this is a string, not a number"
-
 if CLIENT then
     function PART:Use(ply)
-        IDoNotExistLspErrorProbe()
         self.exterior:NotifyEvent(false, true)
     end
 end
